71a29553940e047679583b5034fbf844bd266f3f
olser <gritsienko.oleg@mail.ru>
2022-11-10 13:25:03 +0300

    style and switch corrections

--- src/utils/getTransfersString.ts
+++ src/utils/getTransfersString.ts
@@ -1,14 +1,12 @@
 export const getTransfersString = (length: number) => {
-  if (length === 0) {
-    return 'нет пересадок '
-  }
-  if (length === 1) {
-    return `${length} пересадка`
-  }
-  if (length > 1 && length < 5) {
-    return `${length} пересадки`
-  }
-  if (length > 4) {
-    return `${length} пересадок`
+  switch (true) {
+    case length === 0:
+      return 'нет пересадок '
+    case length === 1:
+      return `${length} пересадка`
+    case length > 1 && length < 5:
+      return `${length} пересадки`
+    case length > 4:
+      return `${length} пересадок`
   }
 }
